02bdd6e7d0a6b898db53d931f35c666f128769bb
pmirski <lukemirski@gmail.com>
2019-03-12 11:38:28 -0700

    Class for linear actuator movement added and tested.

--- Solar_Arduino_Sketches/Solar_arduino/Solar_arduino.ino
+++ Solar_Arduino_Sketches/Solar_arduino/Solar_arduino.ino
@@ -1,6 +1,6 @@
 #include <Azande.h>
-#include "sensors.h"
 #include "motors.h"
+#include "sensors.h"
 
 // Pin Declarations
 int re_pin0 = 2;
@@ -8,6 +8,9 @@ int re_pin1 = 3;
 int re_pin2 = 4;
 int re_pin3 = 5;
 
+int Megamoto_EnablePin = 8;       // This was Pin 8 during testing (delete this note after successful integration)
+int PWMPinA = 9;                  // This was Pin 11 during testing (delete this note after successful integration)
+int PWMPinB = 10;                 // This was Pin 3 during testing (delete this note after successful integration)
 
 // Sensors
 Rotary_Encoder rotary_encoder = Rotary_Encoder(re_pin0,re_pin1,re_pin2,re_pin3);
@@ -15,19 +18,21 @@ Rotary_Encoder rotary_encoder = Rotary_Encoder(re_pin0,re_pin1,re_pin2,re_pin3);
 Sensor_Container sensor_container = Sensor_Container();
 
 // Motors
+LINEAR_ACTUATOR altidutinal_actuator = LINEAR_ACTUATOR(Megamoto_EnablePin, PWMPinA, PWMPinB);
 
 Azande azande(Serial);    // The Azande object variable. Using 'Serial' as 'Stream'
 
 void setup() {
-  //Set pin modes
+  Serial.begin(9600);
+  // Set pin modes
   pinMode(re_pin0, INPUT);
   pinMode(re_pin1, INPUT);
   pinMode(re_pin2, INPUT);
   pinMode(re_pin3, INPUT);
-
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
+  pinMode(Megamoto_EnablePin, OUTPUT);    // Enable Megamoto
+  pinMode(PWMPinA, OUTPUT);
+  pinMode(PWMPinB, OUTPUT);               // Set motor outputs
+  
   while (!Serial) {
     delay(1); // will pause Zero, Leonardo, etc until serial console opens
   }
@@ -39,8 +44,8 @@ void setup() {
 
 void loop() {
   sensor_container.run();
-
   Serial.print("Counter "); Serial.println(rotary_encoder.getCurrentAngle());
   
   delay(200);
+
 }
--- Solar_Arduino_Sketches/Solar_arduino/motors.h
+++ Solar_Arduino_Sketches/Solar_arduino/motors.h
@@ -1,11 +1,29 @@
+/*
+  motors.h - Library for linear actuator code.
+*/
 #ifndef MOTORS_H
 #define MOTORS_H
 
-class LINEAR_ACTUATOR{
-  
+#include <Arduino.h>
+
+
+class LINEAR_ACTUATOR {
+  public:
+    LINEAR_ACTUATOR(int Megamoto_EnablePin, int PWMPinA, int PWMPinB);
+    ~LINEAR_ACTUATOR();
+    void forwardActuator();
+    void reverseActuator();
+    void stopActuator();
+  private:
+    int pin_enable;
+    int pin_PWM_A;
+    int pin_PWM_B;
+    int speed_full;
+    int speed_stop;
 };
 
-class AZIMUTH_MOTOR{
-  
+class AZIMUTH_MOTOR {
+
 };
+
 #endif
