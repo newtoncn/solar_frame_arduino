c579d69e24909f594b5e28aade082fe488d08284
newtoncn <newtoncameron@hotmail.com>
2019-03-12 20:29:08 -0700

    Azande stuff

--- Solar_Arduino_Sketches/Solar_arduino/Solar_arduino.ino
+++ Solar_Arduino_Sketches/Solar_arduino/Solar_arduino.ino
@@ -3,27 +3,33 @@
 #include "motors.h"
 
 // Pin Declarations
-int re_pin0 = 2;
-int re_pin1 = 3;
-int re_pin2 = 4;
-int re_pin3 = 5;
-
+int relay_enable = 2;
+int rotary_pin0 = 3;
+int rotary_pin1 = 4;
+int rotary_pin2 = 5;
+int rotary_pin3 = 6;
+int megamoto_enable = 8;
+int motor_controller_A = 9;
+int motor_controller_B = 10;
 
 // Sensors
-Rotary_Encoder rotary_encoder = Rotary_Encoder(re_pin0,re_pin1,re_pin2,re_pin3);
+Rotary_Encoder rotary_encoder = Rotary_Encoder(rotary_pin0,rotary_pin1,rotary_pin2,rotary_pin3);
 
 Sensor_Container sensor_container = Sensor_Container();
 
 // Motors
 
+//Azande
 Azande azande(Serial);    // The Azande object variable. Using 'Serial' as 'Stream'
+
+define_int_event(rotaryPosition, "Azimuth Position", 0 , "degrees" , , , );
 
 void setup() {
   //Set pin modes
-  pinMode(re_pin0, INPUT);
-  pinMode(re_pin1, INPUT);
-  pinMode(re_pin2, INPUT);
-  pinMode(re_pin3, INPUT);
+  pinMode(rotary_pin0, INPUT);
+  pinMode(rotary_pin1, INPUT);
+  pinMode(rotary_pin2, INPUT);
+  pinMode(rotary_pin3, INPUT);
 
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -34,13 +40,17 @@ void setup() {
 
   Serial.println("Serial Connected");
 
+  azande.begin();
+
+  azande.add(rotaryPosition);
+
   sensor_container.addSensor(rotary_encoder);
 }
 
 void loop() {
   sensor_container.run();
 
-  Serial.print("Counter "); Serial.println(rotary_encoder.getCurrentAngle());
+  azande.send(rotaryPosition,rotary_encoder.getCurrentAngle());
   
   delay(200);
 }
--- Solar_Arduino_Sketches/Solar_arduino/sensors.h
+++ Solar_Arduino_Sketches/Solar_arduino/sensors.h
@@ -50,16 +50,43 @@ class Rotary_Encoder : public Sensor {
       current_gray_ = 0;
   }
 
-  virtual void run();
+  virtual void run(){
+      val0_ = digitalRead(pin0_);
+      val1_ = digitalRead(pin1_);
+      val2_ = digitalRead(pin2_);
+      val3_ = digitalRead(pin3_);
+    
+      num_ = val0_ + val1_ * 2 + val2_ * 4 + val3_ * 8;
+    
+      if (prev_gray_ == -1) prev_gray_ = GrayToBinary32(num_);
+      else {
+        current_gray_ = GrayToBinary32(num_);
+        if (prev_gray_ > current_gray_) counter_++;
+        else if (prev_gray_ < current_gray_) counter_--;
+      }
+    
+      prev_gray_ = current_gray_;
+  }
 
-  void startTracking();
+  void startTracking(){
+      counter_ = 0;
+  }
 
-  double getCurrentAngle();
+  double getCurrentAngle(){
+      return counter_; //counter_ * (360 / 16) * (1.0 / GEAR_RATIO);
+  }
 
   private:
   int pin0_,pin1_,pin2_,pin3_,prev_gray_,current_gray_,counter_,num_,val0_,val1_,val2_,val3_;
 
-  unsigned int GrayToBinary32(unsigned int num);
+  unsigned int GrayToBinary32(unsigned int num){
+      num = num ^ (num >> 16);
+      num = num ^ (num >> 8);
+      num = num ^ (num >> 4);
+      num = num ^ (num >> 2);
+      num = num ^ (num >> 1);
+      return num;
+      }
 };
 
 /*
@@ -71,11 +98,18 @@ class Sensor_Container{
   /*
    * Initialize with number of sensors that will be added
    */
-  Sensor_Container();
+  Sensor_Container(){
+      sensor_num_ = 0;
+  }
 
-  void addSensor(Sensor& sensor);
+  void addSensor(Sensor& sensor){
+      sensors_[sensor_num_] = &sensor;
+      sensor_num_++;
+  }
 
-  void run();
+  void run(){
+      for (int a = 0; a < (int)sizeof(sensors_)/sizeof(Sensor*); a++) sensors_[a]->run();
+  }
 
   private:
   int sensor_num_;
