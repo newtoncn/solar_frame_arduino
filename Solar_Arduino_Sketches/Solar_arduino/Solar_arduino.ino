fd30dfebdef070ac6ab79ba99ea2e24ea9c943bd
Cameron Newton <newtoncameron@hotmail.com>
2019-03-15 18:41:56 -0700

    Cameron/initial commit (#4) Compiling Arduino commit, need to test Azande interface and multi-sensor performance
    * Initial Commit
    
    * Adjusted folder structure
    
    * Updated rotary encoder and file structure
    
    * Updated Readme
    
    * Added sensor and motor framework
    
    * Updated rotary encoder
    
    * Updated rotary encoder
    
    * Azande stuff
    
    * IMU integrated into framework
    
    * Fixed initial commit removed .idea
    
    * Compiling arduino commit

--- Solar_Arduino_Sketches/Solar_arduino/Solar_arduino.ino
+++ Solar_Arduino_Sketches/Solar_arduino/Solar_arduino.ino
@@ -8,61 +8,68 @@ int re_pin1 = 3;
 int re_pin2 = 4;
 int re_pin3 = 5;
 
-// Sensors
-Rotary_Encoder rotary_encoder = Rotary_Encoder(re_pin0,re_pin1,re_pin2,re_pin3);
-
-Sensor_Container sensor_container = Sensor_Container();
-
-// Motors
-=======
 int Megamoto_EnablePin = 8;
-int PWMPinA = 9;           
-int PWMPinB = 10;         
+int PWMPinA = 9;
+int PWMPinB = 10;
 
 int currentSensorAPin = A5;
 int currentSensorBPin = A0;
 
 // Sensors
 Rotary_Encoder rotary_encoder = Rotary_Encoder(re_pin0,re_pin1,re_pin2,re_pin3);
+IMU_Sensor imu_sensor = IMU_Sensor();
+Current_Sensor current_sensor_A = Current_Sensor(currentSensorAPin);
+Current_Sensor current_sensor_B = Current_Sensor(currentSensorBPin);
+
 Sensor_Container sensor_container = Sensor_Container();
-//Current_Sensor current_sensor_A = Current_Sensor(currentSensorAPin);  //DOES NOT WORK
-//Current_Sensor current_sensor_B = Current_Sensor(currentSensorBPin);  //DOES NOT WORK
 
 // Motors
 LINEAR_ACTUATOR altidutinal_actuator = LINEAR_ACTUATOR(Megamoto_EnablePin, PWMPinA, PWMPinB);
 
+//Azande
 Azande azande(Serial);    // The Azande object variable. Using 'Serial' as 'Stream'
 
+define_int_event(rotaryPosition,
+"Azimuth Position", 0, "degrees", , , );
+define_double_event(pitchPosition,
+"Azimuth Position", 0, "degrees", , , );
+define_double_event(headingPosition,
+"Azimuth Position", 0, "degrees", , , );
+
 void setup() {
-  //Set pin modes
-=======
-  Serial.begin(9600);
-  // Set pin modes
-  pinMode(re_pin0, INPUT);
-  pinMode(re_pin1, INPUT);
-  pinMode(re_pin2, INPUT);
-  pinMode(re_pin3, INPUT);
+    // Set pin modes
+    pinMode(re_pin0, INPUT);
+    pinMode(re_pin1, INPUT);
+    pinMode(re_pin2, INPUT);
+    pinMode(re_pin3, INPUT);
+    pinMode(Megamoto_EnablePin, OUTPUT);    // Enable Megamoto
+    pinMode(PWMPinA, OUTPUT);
+    pinMode(PWMPinB, OUTPUT);               // Set motor outputs
 
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+    Serial.begin(9600); //May need to adjust Serial values for Azande Interface and IMU sensor
 
-  pinMode(Megamoto_EnablePin, OUTPUT);    // Enable Megamoto
-  pinMode(PWMPinA, OUTPUT);
-  pinMode(PWMPinB, OUTPUT);               // Set motor outputs
-  
-  while (!Serial) {
-    delay(1); // will pause Zero, Leonardo, etc until serial console opens
-  }
+    while (!Serial) {
+        delay(1); // will pause Zero, Leonardo, etc until serial console opens
+    }
 
-  Serial.println("Serial Connected");
+    Serial.println("Serial Connected");
 
-  sensor_container.addSensor(rotary_encoder);
+    azande.begin();
+
+    azande.add(rotaryPosition);
+    azande.add(pitchPosition);
+    azande.add(headingPosition);
+
+    sensor_container.addSensor(rotary_encoder);
+    sensor_container.addSensor(imu_sensor);
 }
 
 void loop() {
-  sensor_container.run();
+    sensor_container.run();
 
-  Serial.print("Counter "); Serial.println(rotary_encoder.getCurrentAngle());
-  
-  delay(200);
+    azande.send(rotaryPosition, rotary_encoder.getCurrentAngle());
+    azande.send(pitchPosition, imu_sensor.getPitch());
+    azande.send(headingPosition, imu_sensor.getHeading());
+
+    delay(200); //Test Different Delays for Azande interface
 }
--- Solar_Arduino_Sketches/Solar_arduino/sensors.cpp
+++ Solar_Arduino_Sketches/Solar_arduino/sensors.cpp
@@ -44,9 +44,40 @@ double Rotary_Encoder::getCurrentAngle()
 // Class Current_Sensor
 double Current_Sensor::getCurrent() 
 {
-  int integerZeroTo1023 = analogRead(pinNum_);   // analogRead reads int value of 0-1023, represent 0 to 5V.
-  double currentValue = integerZeroTo1023*convertIntToVolts*convertVoltsToAmps; // //Current sense output: I = Vc/0.075, where Vc ranges 0 to 2.99V.
-  return currentValue;     
+  return analogReading_*convertIntToVolts*convertVoltsToAmps; // //Current sense output: I = Vc/0.075, where Vc ranges 0 to 2.99V.
+}
+
+void Current_Sensor::run()
+{
+  analogReading_ = analogRead(pinNum_);   // analogRead reads int value of 0-1023, represent 0 to 5V.
+}
+
+//Class IMU Sensor
+void IMU_Sensor::run(){
+  lsm.read();  /* ask it to read in the data */
+
+  /* Get a new sensor event */
+  sensors_event_t a, m, g, temp;
+
+  lsm.getEvent(&a, &m, &g, &temp);
+
+  a_accel_x_ = a.acceleration.x;
+  a_accel_y_ = a.acceleration.y;
+  a_accel_z_ = a.acceleration.z;
+
+  m_guass_x_ = m.magnetic.x; 
+  m_guass_y_ = m.magnetic.y;
+
+  
+}
+
+double IMU_Sensor::getPitch(){
+  return atan2(-a_accel_x_, sqrt(a_accel_y_*a_accel_y_ + a_accel_z_*(a_accel_z_))) * 180/M_PI;
+}
+
+
+double IMU_Sensor::getHeading(){
+  return atan2(m_guass_x_, m_guass_y_) * 180/M_PI;
 }
 
 
